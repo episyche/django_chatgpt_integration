204118e3a0d5a652a1718d1224f40415feb11efa
VishaliArumugam27 <vishalisri1551@gmail.com>
2023-02-21 11:24:06 +0530

    django secret key

--- chatGPT_integration/chatGPT_integration/settings.py
+++ chatGPT_integration/chatGPT_integration/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--_e=m$db%^_!8ak4%20whmfhod9c1&6)u4k2-vo&==e%_#1_y^'
+# SECRET_KEY = 'django-insecure--_e=m$db%^_!8ak4%20whmfhod9c1&6)u4k2-vo&==e%_#1_y^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
